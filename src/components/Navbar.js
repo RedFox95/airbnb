24b40328ef224e033654b23d6308661458096c7b
Ryan Weidling <ryanweidling@gmail.com>
2024-08-08 21:48:45 -0400

    added airbnb logo to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,9 @@ export default function Navbar() {
     return (
         <div className='navbar'>
             <h1>Navbar</h1>
+            <nav>
+                <img className='navbar--logo' src='https://cdn1.iconfinder.com/data/icons/brands-4/512/fi-brands-airbnb-128.png' alt='airbnb logo' />
+            </nav>
         </div>
     )
 }
