446a7fc84a0007109e0d2d86331e797a100836c5
Ryan Weidling <ryanweidling@gmail.com>
2024-08-11 19:52:14 -0400

    style updates, fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,9 @@
-import React from 'react'
+import React from "react"
 
 export default function Navbar() {
     return (
-        <div className='navbar'>
-            <h1>Navbar</h1>
-            <nav>
-                <img className='navbar--logo' src='https://cdn1.iconfinder.com/data/icons/brands-4/512/fi-brands-airbnb-128.png' alt='airbnb logo' />
-            </nav>
-        </div>
+        <nav>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/1280px-Airbnb_Logo_B%C3%A9lo.svg.png" className="nav--logo" />
+        </nav>
     )
 }
